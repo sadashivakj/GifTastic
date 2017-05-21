6967462910d1f7500b76dc9b89ce932a72dc4f98
sadashivakj <sadashivakj@gmail.com>
2017-05-20 22:42:22 -0500

    checking in small html and js changes

--- assets/javascript/gifapi.js
+++ assets/javascript/gifapi.js
@@ -5,78 +5,59 @@
       // This function will render the buttons
       function renderButtons(){
 
-      	console.log("Entering renderButtons function");
-
       	//Clear the existing set of buttons
       	$("#buttons").empty();
 
       	for(var i=0; i<animals.length; i++){
-      		console.log(animals[i]);
-      		var btn = $("<button>");
+      		//var btn = $("<button>");
+                  var btn = $("<button class='btn btn-default'>");
       		btn.addClass("animal");
       		btn.attr("data-name", animals[i]);
       		btn.text(animals[i]);
       		$("#buttons").append(btn);
       	}
-
-      	console.log("Exiting renderButtons function");
       }
 
       //when Add Animal button is clicked 
       $("#add_animal").on("click", function(event){
-      	console.log("entering inside the button clicked event function");
+      	
       	event.preventDefault();
       	//get the input 
-      	var animal = $("#animal_input").val();
-      	console.log("New animal entered by user - "+animal);
-
+      	var animal = $("#animal_input").val().trim();
       	animals.push(animal);
+            $("#animal_input").val("");
       	// Calling renderButtons which handles the processing of our animal array
-      	console.log("Updated animals array - "+animals);
       	renderButtons();
-      	console.log("Exiting button clicked event function");
       })
 
       function displayAnimalGif(){
-      	console.log("Entering displayAnimalGif function");
       	//get the animal button selected by the user 
-      	var animal = $(this).attr("data-name");
-      	console.log("Selected Animal - "+animal);
-
+            var animal = $(this).attr("data-name");
       	//construct queryURL to make gif API call to get 5 gif images
       	//var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+animal;
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             animal + "&api_key=dc6zaTOxFJmzC&limit=5";
 
-      	console.log("queryURL before making ajax call - "+queryURL);
-      	
       	//make the ajax call to gif API
       	$.ajax({
       		url: queryURL,
       		method: "GET"
       	}).done(function(response){ // perform operation on the response
-                  console.log("Got the output from the api call - "+response);
                   var results = response.data;
-                  console.log("results object - "+results);
-                  console.log("number of elements in the array - "+results.length);
+                  //loop through 5 records and populate the image
                   for(var j = 0; j < results.length; j++){
-                        console.log("entering the for loop");
                         var gifDiv = $("<div class='item'>");
                         var rating = results[j].rating;
-                        console.log("rating - "+rating);
                         var p = $("<p>").text("Rating: " +rating);
 
                         //dynamically create an image tag for the animal
                         var animalImage = $("<img>");
-                        console.log("results[j] image object "+results[j]);
-
                         var imageUrl = results[j].images.fixed_height_still.url;
                         var dataStill = results[j].images.fixed_height_still.url;
                         var dataAnimate = results[j].images.fixed_height.url;
-                        var dataState = "still"; 
+                        var dataState = "still";
                         var imgClass ="gif";
 
-                        console.log("imageUrl - "+imageUrl);
                         //add the src tag and assign the gif image url
                         animalImage.attr("src", imageUrl);
                         animalImage.attr("data-still", dataStill);
@@ -84,19 +65,16 @@
                         animalImage.attr("data-state", dataState);
                         animalImage.attr("alt", animal+" image");
                         animalImage.addClass("gif");
-
-                        console.log("animalImage object - "+animalImage);
-
+                        
+                        //prepend the rating data
                         gifDiv.prepend(p);
+                        //prepend the animal image
                         gifDiv.prepend(animalImage);
 
                         //prepend the gif image based on the response 
                         $("#display_gif").prepend(gifDiv);
-
-                        console.log("Going to the next element of the for loop");
                   }
             });
-      	console.log("Exiting displayAnimalGif function");
       }
 
       // get a handle to the image using $(document).on event listeners 
